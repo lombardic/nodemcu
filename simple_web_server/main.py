45f2fb42798376c712081146b0a700acd09d5792
Chris Lombardi <chris.m.lombardi@gmail.com>
2019-10-08 18:42:23 -0700

    Adding lesson10 activity files

--- simple_web_server/main.py
+++ simple_web_server/main.py
@@ -3,6 +3,16 @@ try:
 except:
     import socket
 
+response_404 = """HTTP/1.0 404 NOT FOUND
+
+<h1>404 Not Found</h1>
+"""
+
+response_500 = """HTTP/1.0 500 INTERNAL SERVER ERROR
+
+<h1>500 Internal Server Error</h1>
+"""
+
 response_template = """HTTP/1.0 200 OK
 
 %s
@@ -30,6 +40,16 @@ def time():
 
     return response_template % body
 
+def dummy():
+    body = "This is a dummy endpoint."
+
+    return response_template % body
+
+handlers = {
+        'time': time,
+        'dummy': dummy
+}
+
 def main():
     s = socket.socket()
     ai = socket.getaddrinfo("0.0.0.0", 8080)
@@ -42,7 +62,6 @@ def main():
     print("Listening, connect your browser to http://<this_host>:8080/")
 
     while True:
-        sleep(.5)
         res = s.accept()
         client_s = res[0]
         client_addr = res[1]
@@ -50,7 +69,15 @@ def main():
         print("Request:")
         print(req)
 
-        response = time()
+        try:
+            path = req.decode().split("\r\n")[0].split(" ")[1]
+            handler = handlers[path.strip('/').split('/')[0]]
+            response = handler()
+        except KeyError:
+            response = response_404
+        except Exception as e:
+            response = response_500
+            print(str(e))
 
         client_s.send(b"\r\n".join([line.encode() for line in response.split("\n")]))
 
